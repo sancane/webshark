2f7f3a67c56ad182b1e7142b3dade0bfc2cacc48
Santiago Carot Nemesio <sancane@gmail.com>
2013-05-15 12:41:43 +0200

    Fix comment types

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,15 @@
 $(window).load( function() {
+
   var wireshark = Webshark.Analyzer({
     container: "whireshark",
     handler: DataTablesHandler.Table
   });
 
-  /* Configure DragandDrop module to use wireshark */
+  // Configure DragandDrop module to use wireshark
   DragAndDrop.use(wireshark.loadContent);
 
-  /* Init drag and drop */
+  // Init drag and drop
   var dragdrop = $("#dragdrop_container");
   dragdrop.html(DragAndDrop.html);
+
 });
